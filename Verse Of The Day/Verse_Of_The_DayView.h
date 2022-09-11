59d9aaaf6b79a907b8ab8f0a52a299e33f1cb2f9
Gabriel Lopes <gabriel@gabstep.com.br>
2022-09-11 00:14:30 -0300

    Initial Commit

---
+++ Verse Of The Day/Verse_Of_The_DayView.h
@@ -0,0 +1,12 @@
+//
+//  Verse_Of_The_DayView.h
+//  Verse Of The Day
+//
+//  Created by Gabriel Lopes Ferreira Ramos on 11/09/22.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Verse_Of_The_DayView : ScreenSaverView
+
+@end
---
+++ Verse Of The Day/Verse_Of_The_DayView.m
@@ -0,0 +1,51 @@
+//
+//  Verse_Of_The_DayView.m
+//  Verse Of The Day
+//
+//  Created by Gabriel Lopes Ferreira Ramos on 11/09/22.
+//
+
+#import "Verse_Of_The_DayView.h"
+
+@implementation Verse_Of_The_DayView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
